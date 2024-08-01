1c1c0df83cf1f159f8f66724bbcc9cacf452aa86
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-08-01 22:09:20 +0530

    mouseover

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -15,7 +15,7 @@ function Card(){
                 <div className='w-90 px-3 py-2 bg-zinc-100 rounded'>
                 <h3 className='font-semibold text-xl'>{item.name}</h3>
                 <p className='text-xs mt-2'>{item.description}</p>
-                <button onClick={handleClickDownload} className='px-2 py-1 bg-blue-400 text-xs font-semibold text-zinc-100 rounded mt-3'>Download Now</button>
+                <button onMouseOver={handleClickDownload} className='px-2 py-1 bg-blue-400 text-xs font-semibold text-zinc-100 rounded mt-3'>Download Now</button>
             </div>
             ))}
         </div>
