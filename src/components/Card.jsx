728cd23046d052f60489f8ab3a5ac9ab3b7e3214
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-10-29 03:58:54 +0530

    object state function

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { useState } from 'react';
 
 function App() {
-    const [val, setVal] = useState(100);
+    const [val, setVal] = useState({name: "vishwajeet", isBlocked: "True"});
 
     return (
         <div className='p-4'>
-            <h1>{val}</h1>
-            <button onMouseMoveCapture={()=>setVal(prev=>prev+5)} className='px-20 py-10 mt-2 rounded-full text-xs bg-blue-500 text-black'>Add karo</button>
+            <h1>name : {val.name}</h1>
+            <h2>blocked : {val.isBlocked.toString()}</h2>
+            <button onClick={()=>setVal({...val, isBlocked: !val.isBlocked})} className='px-2 py-1 mt-2 rounded-full text-xs bg-blue-500 text-black'>Kya mein block hu ?</button>
         </div>
     );  
 }
