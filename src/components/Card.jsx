47bf3fe9e59fb023b7c25fff7fab30a7a2f055a1
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-10-29 03:01:52 +0530

    infinite change true false problem solve

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,7 @@ function App() {
     return (
         <div className='p-4'>
             <h1>{banned.toString()}</h1>
-            <button onClick={()=>setBanned("True")} className='px-2 py-1 mt-2 rounded-full text-xs bg-blue-500 text-black'>Ban karo</button>
+            <button onClick={()=>setBanned(!banned)} className='px-2 py-1 mt-2 rounded-full text-xs bg-blue-500 text-black'>Ban karo</button>
         </div>
     );  
 }
