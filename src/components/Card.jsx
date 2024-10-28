78f4d38d5ca8b3c5e5d792bba85d8d9bb43bb8ba
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-10-29 02:30:34 +0530

    efficient use state code for member1

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { useState } from 'react';
 
 function App() {
-    const ans = useState({name:"vishwajeet"});
-    console.log(ans[0]);
+    const [score, setScore] = useState(108000);
+
     return (
         <div>
-            App
+            {score}
         </div>
     );  
 }
