639d42798123ebd077898fd4fb72b1dc67573821
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-10-29 02:45:32 +0530

    state member 2

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,8 +5,9 @@ function App() {
     const [score, setScore] = useState(108000);
 
     return (
-        <div>
-            {score}
+        <div className='p-4'>
+            <h1>{score}</h1>
+            <button onClick={()=>setScore(200)} className='px-2 py-1 mt-2 rounded-full text-xs bg-blue-500 text-black'>Change</button>
         </div>
     );  
 }
