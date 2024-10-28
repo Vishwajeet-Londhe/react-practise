78b26f80d3eb7d3abc2cc37fe076cbcfcb1a7349
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-10-29 02:10:23 +0530

    object with value in state

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useState } from 'react';
 
 function App() {
-    const ans = useState(false);
+    const ans = useState({name:"vishwajeet"});
     console.log(ans[0]);
     return (
         <div>
