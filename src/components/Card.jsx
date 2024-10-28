91a450080a0cda0b3167dcec510e9ebe0e46291d
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-10-29 03:22:55 +0530

    added in previous

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { useState } from 'react';
 
 function App() {
-    const [banned, setBanned] = useState(false);
+    const [val, setVal] = useState(100);
 
     return (
         <div className='p-4'>
-            <h1>{banned.toString()}</h1>
-            <button onClick={()=>setBanned(!banned)} className='px-2 py-1 mt-2 rounded-full text-xs bg-blue-500 text-black'>Ban karo</button>
+            <h1>{val}</h1>
+            <button onMouseMoveCapture={()=>setVal(prev=>prev+5)} className='px-20 py-10 mt-2 rounded-full text-xs bg-blue-500 text-black'>Add karo</button>
         </div>
     );  
 }
