762bcb5c08368387ea0662a83527414d31e09a30
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-10-30 21:44:53 +0530

    not in ternary

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,7 +8,7 @@ function App() {
         <div className='p-4'>
             <h1>name : {val.name}</h1>
             <h2>blocked : {val.isBlocked.toString()}</h2>
-            <button onClick={()=>setVal({...val, isBlocked: !val.isBlocked})} className={`px-2 py-1 mt-2 rounded-full text-x ${val.isBlocked ? "bg-blue-500" : "bg-red-500"} text-black`}>Kya mein block hu ?</button>
+            <button onClick={()=>setVal({...val, isBlocked: !val.isBlocked})} className={`px-2 py-1 mt-2 rounded-full text-x ${!val.isBlocked ? "bg-blue-500" : "bg-red-500"} text-black`}>Kya mein block hu ?</button>
         </div>
     );  
 }
