0339518f27251d5d2ddb79edf1daa6b83bd81f6f
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-11-01 12:27:42 +0530

    removed 3 from array

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,22 @@
+// import React from 'react'
+// import { useState } from 'react'
+
+// function App() {
+//     const [val,setVal]=useState([1,2,3,4,5,6])
+
+//   return (
+//     <div className='p-5'>
+//         {val.map(item=><h1>{item}</h1>)}
+//         <button onClick={()=>setVal(()=>val.filter((item,index)=> index!=val.length - 1))} className='px-2 py-1 text-xs text-white rounded-full bg-blue-500'>remove item</button>
+      
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+// removing 3
 import React from 'react'
 import { useState } from 'react'
 
@@ -7,7 +26,7 @@ function App() {
   return (
     <div className='p-5'>
         {val.map(item=><h1>{item}</h1>)}
-        <button onClick={()=>setVal(()=>val.filter((item,index)=> index!=val.length - 1))} className='px-2 py-1 text-xs text-white rounded-full bg-blue-500'>remove item</button>
+        <button onClick={()=>setVal(()=>val.filter((item,index)=> index!=2))} className='px-2 py-1 text-xs text-white rounded-full bg-blue-500'>remove item</button>
       
     </div>
   )
