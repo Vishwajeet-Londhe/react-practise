97802a3cf29011a5e6e0984a78a2dc3c50dc4316
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-11-01 12:38:55 +0530

    adding 7 in array

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -34,6 +34,24 @@
 
 // export default App
 // divisible by 2 removed
+// import React from 'react'
+// import { useState } from 'react'
+
+// function App() {
+//     const [val,setVal]=useState([1,2,3,4,5,6])
+
+//   return (
+//     <div className='p-5'>
+//         {val.map(item=><h1>{item}</h1>)}
+//         <button onClick={()=>setVal(()=>val.filter((item)=> item%2!==0))} className='px-2 py-1 text-xs text-white rounded-full bg-blue-500'>remove item</button>
+      
+//     </div>
+//   )
+// }
+
+// export default App
+
+// adding 7 in Array
 import React from 'react'
 import { useState } from 'react'
 
@@ -43,7 +61,7 @@ function App() {
   return (
     <div className='p-5'>
         {val.map(item=><h1>{item}</h1>)}
-        <button onClick={()=>setVal(()=>val.filter((item)=> item%2!==0))} className='px-2 py-1 text-xs text-white rounded-full bg-blue-500'>remove item</button>
+        <button onClick={()=>setVal([...val,7])} className='px-2 py-1 text-xs text-white rounded-full bg-blue-500'>click</button>
       
     </div>
   )
