5fd2fc22b292c35b1b418dbc7ba0d4f52a5ed34c
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-11-02 19:12:11 +0530

    method 1 but not efficient

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -69,27 +69,47 @@
 
 // export default App
 
-// change age of anu
+// change age of akanksha
+
+// import React from 'react'
+// import { useState } from 'react'
+
+// function App() {
+//     const [val,setVal]= useState([{name:"anu",age:19}, {name:"vishwajeet",age:21}, {name:"akanksha",age:24}]);
+
+//   return (
+//     <div className='p-5'>
+//         {val.map((item)=> (
+//           <div>
+//             <h1>{item.name}</h1>
+//             <h2>{item.age}</h2>
+//           </div> 
+//         ))}
+//         <button
+//          onClick={()=>setVal(()=>val.map(item=>item.name==="akanksha" ? ({name:"akanksha",age:20}) : item))} className='px-2 py-1 text-xs text-white rounded-full bg-blue-500'>click</button>
+      
+//     </div>
+//   )
+// }
+
+// export default App
+
+// if val == false print "bahar jaao" & if val == true print "maat jaao na"
 
 import React from 'react'
 import { useState } from 'react'
 
-function App() {
-    const [val,setVal]= useState([{name:"anu",age:19}, {name:"vishwajeet",age:21}, {name:"akanksha",age:24}]);
-
+function Card() {
+  const[val,setVal]=useState(false);
   return (
-    <div className='p-5'>
-        {val.map((item)=> (
-          <div>
-            <h1>{item.name}</h1>
-            <h2>{item.age}</h2>
-          </div> 
-        ))}
-        <button
-         onClick={()=>setVal(()=>val.map(item=>item.name==="akanksha" ? ({name:"akanksha",age:20}) : item))} className='px-2 py-1 text-xs text-white rounded-full bg-blue-500'>click</button>
+    <div>
+      <h1>
+        {val===false ? "BAHAAR jaao" : "MAAT jaao Na"}
+      </h1>
+      <button onClick={()=>{setVal(!val)}} className='px-2 py-1 bg-blue-500 rounded-full'>Change krr be </button>
       
     </div>
   )
 }
 
-export default App
+export default Card
