8fd6f9341e20ab4350bfe10fefbb6d84b587e1e8
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-11-05 23:59:18 +0530

    added more buttons

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ function App() {
     <>
       <Card text="Know More" color="bg-blue-600"/>
       <Card text="Download" color="bg-red-700"/>
+      <Card text="Watch" color="bg-orange-700"/>
+      <Card text="About us" color="bg-green-700"/>
+      <Card text="FeedBack" color="bg-yellow-700"/>
     </>
   );
 }
