d450a19aca5ddbcd272299cfb4f1291fe75e31f6
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-11-20 23:48:50 +0530

    name done

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -153,14 +153,14 @@
   
   import React from 'react'
   
-  function Card({image}) {
+  function Card({image,name}) {
     return (
       <div className='w-52 bg-white rounded-md overflow-hidden'>
         <div className='w-full h-32 bg-sky-200'>
           <img className='w-full h-full object-cover object-top' src={image} alt="" />
         </div>
         <div className='w-full p-3'>
-          <h3 className='text-xl font-semibold'>Vishwajeet</h3>
+          <h3 className='text-xl font-semibold'>{name}</h3>
           <h5 className='text-xs'>Profession</h5>
           <button className='mt-4 px-3 py-1 text-xs text-white bg-blue-500 font-semibold rounded-md'>Add Frined</button>
         </div>
