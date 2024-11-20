2bdb3079027e58f28b12c99b97e8e4d918d47862
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-11-20 23:50:52 +0530

    profession done

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -153,7 +153,7 @@
   
   import React from 'react'
   
-  function Card({image,name}) {
+  function Card({image,name,profession}) {
     return (
       <div className='w-52 bg-white rounded-md overflow-hidden'>
         <div className='w-full h-32 bg-sky-200'>
@@ -161,7 +161,7 @@
         </div>
         <div className='w-full p-3'>
           <h3 className='text-xl font-semibold'>{name}</h3>
-          <h5 className='text-xs'>Profession</h5>
+          <h5 className='text-xs'>{profession}</h5>
           <button className='mt-4 px-3 py-1 text-xs text-white bg-blue-500 font-semibold rounded-md'>Add Frined</button>
         </div>
 
